b5b5aa63f6648f30865124bef5d9a66bed5dc0b2
David THIBAULT <david.thibault@adways.com>
2021-04-14 10:18:24 +0200

    correction migration pour être compatible mysql

--- src/Migrations/Version20200113234049.php
+++ src/Migrations/Version20200113234049.php
@@ -20,16 +20,16 @@ final class Version20200113234049 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
-
-        $this->addSql('CREATE TABLE comment (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(255) NOT NULL, content CLOB NOT NULL, post_id INTEGER DEFAULT NULL)');
-        $this->addSql('CREATE TABLE post (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, content CLOB NOT NULL, created_at DATETIME NOT NULL)');
+        //$this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+        
+        $this->addSql('CREATE TABLE comment (id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, content TEXT NOT NULL, post_id INTEGER DEFAULT NULL)');
+        $this->addSql('CREATE TABLE post (id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, content TEXT NOT NULL, created_at DATETIME NOT NULL)');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+        //$this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
         $this->addSql('DROP TABLE comment');
         $this->addSql('DROP TABLE post');
